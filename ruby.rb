de50499e6bec2300b3a5e82c701467e03caad82a
onifernando1 <onifernando1@gmail.com>
2022-07-12 16:53:42 +0100

    Random word method

---
+++ ruby.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+
+class Game
+  def random_word
+    fname = 'google-10000-english-no-swears.txt'
+    randnum = rand(1..9893)
+    word = ''
+
+    File.open(fname, 'r') do |file|
+      file.readlines.each_with_index do |line, idx|
+        word = line if idx == randnum
+      end
+    end
+
+    puts word
+  end
+end
+
+class Player < Game
+end
+
+game = Game.new()
+game.random_word
