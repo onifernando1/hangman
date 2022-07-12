1f29f74128a308366af83206dc93f250c375d65f
onifernando1 <onifernando1@gmail.com>
2022-07-12 17:07:25 +0100

    Ask letters until game won or guesses left 0

--- ruby.rb
+++ ruby.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Game
+  attr_reader :word
+
   def random_word
     fname = 'google-10000-english-no-swears.txt'
     randnum = rand(1..9893)
@@ -18,31 +20,35 @@ class Game
 end
 
 class Player < Game
+  attr_reader :player_guess, :guesses
 
-    def initialize
-        @guesses = 12
-        @player_guess = "AB"
-    end 
+  def initialize
+    @guesses = 12
+    @player_guess = 'AB'
+  end
 
-    def guesses
-        puts "Guesses remaining: #{@guesses}"
-    end 
+  def guesses
+    puts "Guesses remaining: #{@guesses}"
+  end
 
-    def player_guess
-        puts "Guess a letter"
-        until @player_guess.length == 1 
-            @player_guess = gets.chomp
-            if @player_guess.length > 1 || @player_guess.length < 1 
-                puts "INVALID"
-            end 
-        end  
-        @guesses -= 1 
-    end 
-
+  def player_guess
+    puts 'Guess a letter'
+    @player_guess = gets.chomp
+    if @player_guess.length > 1 || @player_guess.empty?
+      puts 'INVALID'
+      @player_guess = gets.chomp
+      guesses()
+    elsif @player_guess.length == 1
+      @guesses -= 1
+      guesses()
+    end
+    @player_guess
+  end
 end
 
 game = Game.new
 game.random_word
-player = Player.new()
-player.player_guess
-player.guesses
+player = Player.new
+until player.player_guess == game.word || player.guesses == 0
+  player.player_guess
+end
